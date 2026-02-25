0b4dd12edc9a54d1cd515ba4ad7acb08fa565551
rowan <rowanajmarshall@gmail.com>
2026-02-25 18:08:20 +0000

    moar fix

--- src/worker.js
+++ src/worker.js
@@ -5,9 +5,7 @@ export default {
         // Serve area-page.html for /area/:code routes while keeping the browser URL intact.
         // This avoids Cloudflare's pretty-URL redirect behaviour that strips the postcode.
         if (/^\/area\/[^/]+\/?$/.test(url.pathname)) {
-            return env.ASSETS.fetch(
-                new Request(new URL('/area-page.html', url).toString(), request)
-            );
+            return env.ASSETS.fetch(url.origin + '/area-page.html');
         }
 
         return env.ASSETS.fetch(request);
